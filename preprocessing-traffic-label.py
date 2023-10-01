99e053a7c6ff9ef3e36cee329e7a380a46223991
mehdiseydali <142658073+mehdiseydali@users.noreply.github.com>
2023-10-01 03:39:42 -0700

    Update preprocessing-traffic-label.py

--- preprocessing-traffic-label.py
+++ preprocessing-traffic-label.py
@@ -1,7 +1,87 @@
 import os
 import csv
 from scapy.all import *
+def get_application_from_filename(filename):
+  """Gets the application of a packet from the filename.
 
+  Args:
+    filename: The filename of the packet.
+
+  Returns:
+    The application of the packet, or None if the application cannot be determined.
+  """
+
+  # Get the prefix of the filename.
+  prefix = filename.split(".")[0].lower()
+
+  # Determine the application based on the prefix.
+  if prefix == "Icq":
+    application = "Icq"
+  elif prefix == "Chat_facebook":
+    application = "Chat_facebook"
+  elif prefix == "Chat_Hangout":
+    application = "Chat_Hangout"
+  elif prefix == "Chat_gmail":
+    application = "Chat_gmail"
+  elif prefix == "Chat_Skype":
+    application = "Chat_Skype"
+  elif prefix == "Email":
+    application = "Email"
+  elif prefix == "Gmail":
+    application = "Gmail"
+  elif prefix == "FTPS":
+    application = "FTPS"
+  elif prefix == "SFTP":
+    application = "SFTP"
+  elif prefix == "SCP":
+    application = "SCP"
+  elif prefix == "FTP_SKype":
+    application = "FTP_SKype"
+  elif prefix == "Torrent":
+    application = "Torrent"
+  elif prefix == "Yuotube":
+    application = "Yuotube"
+  elif prefix == "Netflix":
+    application = "Netflix"
+  elif prefix == "Spotify":
+    application = "Spotify"
+  elif prefix == "Vimo":
+    application = "Vimo"    
+  elif prefix == "Streamig_Skype":
+    application = "Streamig_Skype"
+  elif prefix == "Voip_Skype":
+    application = "Voip_Skype"
+  elif prefix == "Voipbuster":
+    application = "Voipbuster"
+  elif prefix == "Voip_Hangout":
+    application = "Voip_Hangout"
+  elif prefix == "Voip_facebook":
+    application = "Voip_facebook"    
+  else:
+    application = None
+
+  return application
+
+def get_label(filename):
+  """Gets the label of a packet from the filename.
+
+  Args:
+    filename: The filename of the packet.
+
+  Returns:
+    The label of the packet, or None if the label cannot be determined.
+  """
+
+  # Get the prefix of the filename.
+  prefix = filename.split(".")[0].lower()
+
+  # Determine the label based on the prefix.
+  if prefix.startswith("vpn_"):
+    label = "VPN"
+  else:
+    label = "Non-VPN"
+
+  return label
 # Function to mask the IP layer header
 def ip_mask(packet):
     if IP in packet:
@@ -72,6 +152,8 @@ csv_writer = csv.writer(csv_file)
 for pcap_file in os.listdir(pcap_directory):
     if pcap_file.endswith('.pcap'):
         label = categorize_file_type(pcap_file)
+        application = get_application_from_filename(pcap_path.name)
+        app_label = get_label(pcap_path.name)
         if label is not None:
             packets = rdpcap(os.path.join(pcap_directory, pcap_file))
 
@@ -81,7 +163,7 @@ for pcap_file in os.listdir(pcap_directory):
                     normalized_packets = split_and_pad(packet)
                     for normalized_packet in normalized_packets:
                         normalize_packet(normalized_packet)
-                        csv_writer.writerow([bytes(normalized_packet), label])
+                        csv_writer.writerow([bytes(normalized_packet), label,application+app_label])
 
 # Close the CSV file
 csv_file.close()
