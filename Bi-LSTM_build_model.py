7b7660f6f16b140e6211071f13e910915a6c6d46
mehdiseydali <142658073+mehdiseydali@users.noreply.github.com>
2023-10-06 04:11:29 -0700

    Update Bi-LSTM_build_model.py

--- Bi-LSTM_build_model.py
+++ Bi-LSTM_build_model.py
@@ -6,18 +6,24 @@ def BiLSTM_build_model(params):
 
   # Add the Bi-LSTM layers
   model.add(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(params['input_shape'][1], return_sequences=True), input_shape=params['input_shape']))
+  model.add(tf.keras.layers.BatchNormalization())
   model.add(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(params['input_shape'][1], return_sequences=True)))
+  model.add(tf.keras.layers.BatchNormalization())
   model.add(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(params['input_shape'][1], return_sequences=True)))
+  model.add(tf.keras.layers.BatchNormalization()) 
 
   # Add the attention layer
   model.add(tf.keras.layers.Attention())
 
   # Add the fully connected layers (MLP)
   model.add(tf.keras.layers.Dense(params['dense_neurons'][0], activation=params['activation']))
+  model.add(tf.keras.layers.BatchNormalization())
   model.add(tf.keras.layers.Dense(params['dense_neurons'][1], activation=params['activation']))
+  model.add(tf.keras.layers.BatchNormalization())
   model.add(tf.keras.layers.Dense(params['dense_neurons'][2], activation=params['activation']))
+  model.add(tf.keras.layers.BatchNormalization()) 
   model.add(tf.keras.layers.Dense(params['dense_neurons'][3], activation=params['activation']))
-
+  model.add(tf.keras.layers.BatchNormalization())  
   # Save the model
   model.save('bilstm_model.h5')
    
